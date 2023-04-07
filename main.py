1f586c10a3d4cde64353fc6e60b25b252c7c750e
baek0126 <130022709+baek0126@users.noreply.github.com>
2023-04-07 11:17:00 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,41 @@
+# add
+
+def main():
+    print("Let's implement addition. Type two numbers for x and y.")
+
+    x = int(input("x > "))
+    y = int(input("y > "))
+
+    print("%d + %d = %d" % (x, y, add(x, y)))
+
+
+def add(x,y):
+    return x+y
+
+if __name__ == "__main__":
+    main()
+
+
+# divide
+
+def main():
+    print("Let's implement division. Type two numbers for x and y")
+
+    x = int(input("x > "))
+    y = int(input("y > "))
+
+    print("%d / %d = %0.3f" % (x, y, divide(x, y)))
+
+
+def add(a, b):
+    return a + b
+
+
+def divide(x,y):
+    if y == 0:
+        print("Error: cannot divide by zero.")
+    else:
+        return x/y
+
+if __name__ == "__main__":
+    main()
